b45a62f8fde9b419bf6bc9097886ecd28f72d052
Oliver Hine <git@oliy.co.uk>
2017-03-11 19:33:20 +0000

    more slides

--- scratch.clj
+++ scratch.clj
@@ -28,3 +28,31 @@
 (defn fib
   ([] (fib 0 1))
   ([i j] (lazy-seq (cons i (fib j (+ i j))))))
+
+
+(defschema Foo {:a [Int]
+                :b Str})
+
+(validate Foo {:a "not numbers"
+               :b :not-a-string})
+
+;; => ExceptionInfo Value does not match schema:
+;;    {:a (not (sequential? "not numbers")),
+;;     :b (not (instance? java.lang.String :not-a-string))}
+
+(generate {:a Int :b Str})
+
+[{:a []
+  :b ""}
+
+ {:a [-18]
+  :b ""}
+
+ {:a [876545 -9776]
+  :b "-a90%bD!"}
+
+ {:a [122176 0 8391213001]
+  :b ".{7s_kdqW;iYf ywV39*"}
+
+ {:a [9 65342 9879634211 -31 1434988905 411 -65681247 ... ]
+  :b "      -  \OC6 daQ @'`sgeR ¬| f_bdA ... "}]
